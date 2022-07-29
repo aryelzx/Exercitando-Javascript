98554c1569bce9fc9633fd218009a985ac7c713c
aryelzx <arielcearamor102@gmail.com>
2022-07-29 16:48:14 -0300

    urna em js

---
+++ etapas.js
@@ -0,0 +1,51 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+            numero: '38111',
+            nome: 'Xico',
+            partido: 'PSDB',
+            fotos:[
+                {url:'vereador1.jpeg', legenda: 'Vereador'}
+            ]
+        },
+        {
+            numero: '77222',
+            nome: 'Zé',
+            partido: 'PT',
+            fotos:[
+                {url:'vereador2.jpeg', legenda: 'Vereador'}
+                  ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Mendoza',
+                partido: 'CSC',
+                vice: 'Vina',
+                fotos:[
+                    {url:'mendoza.jpeg', legenda:'Prefeito'},
+                    {url:'vina.jpeg', legenda:'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Tico',
+                partido: 'CCC',
+                vice: 'Teco',
+                fotos:[
+                    {url:'tico.jpeg', legenda:'Prefeito'},
+                    {url:'teco.jpeg', legenda:'Vice-Prefeito', small: true}
+                ]
+            },
+            
+        ]
+    }
+];
